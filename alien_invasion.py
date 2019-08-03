5222f11ae36efb164097ef34f432989a54c3cf8e
sarthoksarik <kirasnamdas@gmail.com>
2019-08-03 21:04:37 +0600

    dropping the fleet and changing the direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,6 +81,7 @@ class AlienInvasion:
                  self.bullets.remove(bullet)
     def _update_aliens(self):
         """update the position of all aliens in fleet"""
+        self._check_fleet_edges()
         self.aliens.update() 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -111,7 +112,19 @@ class AlienInvasion:
         self.aliens.add(alien)
         alien.rect.x = alien.x
         alien.rect.y =  alien.rect.height + 2 * alien.rect.height * row_number
-        self.aliens.add(alien)   
+        self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self):
+        """drop the entire fleet and change fleet direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1   
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
--- settings.py
+++ settings.py
@@ -20,3 +20,7 @@ class Settings:
 
         #alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+
+        #fleet_direction of 1 represents right; - represents left
+        self.fleet_direction = 1
