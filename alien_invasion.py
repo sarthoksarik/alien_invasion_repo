82482e4817641c7872595b2c03e22d29c66e3778
sarthoksarik <kirasnamdas@gmail.com>
2019-08-06 12:08:35 +0600

    identifying which part of the game should run

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,12 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
 
     def _check_events(self):
@@ -130,7 +133,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
-            
+
 
 
     def _create_fleet(self):
